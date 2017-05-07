7ff15af09a529abe4350257996493bdb79899cae
Louis Morgan <ljrmorgan@gmail.com>
2017-05-07 12:36:47 +0100

    Add code coverage with coveralls

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: [
+       "coveralls": :test,
+       "coveralls.detail": :test,
+       "coveralls.post": :test,
+       "coveralls.html": :test,
+     ],
      deps: deps()]
   end
 
@@ -35,7 +42,8 @@ defmodule Issues.Mixfile do
       {:httpoison, "~> 0.11"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.15"},
-      {:earmark, "~> 1.2"}
+      {:earmark, "~> 1.2"},
+      {:excoveralls, "~> 0.6", only: :test}
     ]
   end
 
